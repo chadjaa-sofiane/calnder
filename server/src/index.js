b89df97d41e7b94da924d9aa13f2769f52c00399
Chadjaa Sofianne <67962574+chadjaa-sofiane@users.noreply.github.com>
2022-06-14 19:24:57 +0100

    change data structure

--- server/src/index.js
+++ server/src/index.js
@@ -12,34 +12,36 @@ app.use(express.urlencoded({ extended: true }));
 const list = [
     {
         id: 1,
-        date: "2020-01-01",
+        date: "Sun, Jun 26th, 2022",
         note: "This is a note"
     },
     {
         id: 2,
-        date: "2020-01-02",
+        date: "Mon, Jun 27th, 2022",
         note: "This is a note"
     },
     {
         id: 3,
-        date: "2020-01-03",
+        date: "Sun, Jun 26th, 2022",
         note: "This is a note"
     }
 ];
 
-app.get("api/calender/add", (req, res) => {
-    const { date, Note } = req.body;
-    if (!date || !Note) {
+app.get("/api/calender/allNotes", (_, res) => {
+    res.json(list);
+})
+
+
+app.post("api/calender/addNote", (req, res) => {
+    const { date, note } = req.body;
+    if (!date || !note) {
         return res.status(400).json({
             error: "Bad Request"
         })
     }
-    list.push({ id: list.length + 1, date, Note });
+    list.push({ id: list.length + 1, date, note });
 })
 
-app.get("/api/calender/get", (_, res) => {
-    res.json(list);
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
